95e236eeaaaeab4836c10c803cdcc4adb07fc361
asishraz <iamasish3001@gmail.com>
2021-05-31 18:58:12 +0530

    components added react router installed

--- ecart_app/desi_ekart/src/App.js
+++ ecart_app/desi_ekart/src/App.js
@@ -1,21 +1,29 @@
 import logo from "./logo.svg";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Details from "./components/Details";
+import Default from "./components/Default";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">Column 1</div>
-          <div className="col-6">
-            <span>
-              <i className="fas fa-home"></i>
-            </span>
-          </div>
-        </div>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/details" component={Details} />
+            <Route path="/" component={ProductList} />
+            <Route path="/cart" component={Cart} />
+            <Route component={Default} />
+          </Switch>
+        </BrowserRouter>
+      </React.Fragment>
     );
   }
 }
